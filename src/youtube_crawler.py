6d28a5141688ee773af85aec0429806318ece5cc
Ktoxz <69800528+ktoxz@users.noreply.github.com>
2026-01-16 00:34:38 +0700

    feat: add yotube_crawler

---
+++ src/youtube_crawler.py
@@ -0,0 +1,50 @@
+from googleapiclient.discovery import build
+import numpy as np
+import pandas as pd
+# Cấu hình API
+API_KEY = "AIzaSyC7czQVblYp3xv6SRbEw2J-0xhV1lSBEjk"
+VIDEO_ID = "52NiIC3gJnE" # Thay bằng ID video của bạn
+
+def get_youtube_comments(video_id):
+    youtube = build("youtube", "v3", developerKey=API_KEY)
+    comments = []
+    next_page_token = None
+
+    while True:
+        # Gọi API lấy danh sách comment
+        request = youtube.commentThreads().list(
+            part="snippet",
+            videoId=video_id,
+            maxResults=100, # Tối đa 100 comment mỗi lần gọi
+            pageToken=next_page_token,
+            textFormat="plainText"
+        )
+        response = request.execute()
+
+        # Trích xuất dữ liệu từ JSON
+        for item in response.get("items", []):
+            comment = item["snippet"]["topLevelComment"]["snippet"]
+            comments.append({
+                "text": f"{comment["textDisplay"]}"
+            })
+
+        # Kiểm tra nếu còn trang tiếp theo
+        next_page_token = response.get("nextPageToken")
+        if not next_page_token:
+            break
+
+    return comments
+
+# Chạy và in kết quả
+all_comments = get_youtube_comments(VIDEO_ID)
+print(f"Tổng số comment lấy được: {len(all_comments)}")
+
+for c in all_comments[:5]: # In thử 5 comment đầu tiên
+    print(f"{c['text']}")
+    
+cmt = pd.DataFrame(all_comments)
+cmt.to_csv(f"{VIDEO_ID}.csv")
+
+
+# Labels:
+# 
